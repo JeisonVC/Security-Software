37213f47c547508d3b4f4f9586d00cde305da123
Jhon <jf29@misena.edu.co>
2016-06-04 15:35:16 -0500

    Act

--- WEBAPP/Views/index.php
+++ WEBAPP/Views/index.php
@@ -3,18 +3,24 @@
   <head>
     <meta charset="utf-8">
     <title>SecuritySoftware</title>
+    <link rel="stylesheet" href="Styles/estilos.css" charset="utf-8">
   </head>
+
   <body>
-    <h1>Ingreso al Sistema</h1>
-    <form action="validar.php" method="post" >
+
+    <form action="validar.php" method="post">
+      <h3>Ingresar</h3>
       <input type="text" name="nombre" placeholder="Escriba Nombre">
       <br>
       <input type="password" name="contrasena" placeholder="Escriba Contraseña">
-      <br><br>
+      <br>
+
 
       <input type="submit" name="inicio" value="Iniciar Sesion">
       <input type="reset" name="borrar" value="Limpiar!">
     </form>
+
+    <a href="#"><img src="" alt="" /></a>
 
   </body>
 </html>
