a04fe1e21201c4b29279c57a952c6a8ecd96b3f2
Jhon <jf29@misena.edu.co>
2016-07-23 12:21:30 -0500

    ...

--- WEBAPP/Views/validar.class.php
+++ WEBAPP/Views/validar.class.php
@@ -1,63 +1,22 @@
 <?php
 class login{
-
-
-public static function valida($name,$pass){
-  $contador=0;
-  try {
-    $pdo=Conexion::Abrirbd();
-
-  $verificar="SELECT usu_nick,usu_pass FROM usuario WHERE usu_nick=?";
-
-    $mostrar=$pdo->prepare($verificar);
-
-    $mostrar->execute(array($name));
-
-    while($dato=$mostrar->fetch(PDO::FETCH_ASSOC)){
-      if (password_verify($pass,$dato["usu_pass"])) {
-        $contador++;
-      }
-    }
-    if ($contador>0) {
-      session_start();
-      $_SESSION["nombre"]=$_POST["nombre"];
-      header("location: inicio.php");
-    }
-    else {
-      echo "Error en los datos";
-    }
-    Conexion::Cerrarbd();
-  }
-
-  catch (Exception $e) {
-      echo "Error: " . $e->getMessage() . " en la linea" . $e->getLine() . "<br>";
-      echo "El codigo del error es: " . $e->getCode();
-  }
-}
-}
-
-  /*class login{
-    private static $verificar;
-    private static $mostrar;
-    private static $dato;
-    private static $contador=0;
-
-    public static function valida($name,$pass){
+  public static function valida($name,$pass){
+    $contador=0;
       try {
-        $pdo=Conexion::Abrirbd();
+          $pdo=Conexion::Abrirbd();
 
-        self::$verificar="SELECT usu_nick,usu_pass FROM usuario WHERE usu_nick=:nick";
+          $verificar="SELECT usu_nick,usu_pass FROM usuario WHERE usu_nick=?";
 
-        self::$mostrar=$pdo->prepare(self::$verificar);
-        self::$mostrar->bindValue(":nick",$name);
-        self::$mostrar->execute();
+          $mostrar=$pdo->prepare($verificar);
 
-        while(self::$dato=self::$mostrar->fetch(PDO::FETCH_ASSOC)){
-          if (password_verify($pass,self::$dato["usu_pass"])) {
-    			  self::$contador++;
-    			}
-    	  }
-        if (self::$contador>0) {
+          $mostrar->execute(array($name));
+
+          while($dato=$mostrar->fetch(PDO::FETCH_ASSOC)){
+            if (password_verify($pass,$dato["usu_pass"])) {
+              $contador++;
+            }
+          }
+        if ($contador>0) {
           session_start();
           $_SESSION["nombre"]=$_POST["nombre"];
           header("location: inicio.php");
@@ -67,11 +26,10 @@ public static function valida($name,$pass){
         }
         Conexion::Cerrarbd();
       }
-
-      catch (Exception $e) {
-          echo "Error: " . $e->getMessage() . " en la linea" . $e->getLine() . "<br>";
-          echo "El codigo del error es: " . $e->getCode();
-      }
+    catch (Exception $e) {
+      echo "Error: " . $e->getMessage() . " en la linea" . $e->getLine() . "<br>";
+      echo "El codigo del error es: " . $e->getCode();
     }
-  }*/
+  }
+}
 ?>
